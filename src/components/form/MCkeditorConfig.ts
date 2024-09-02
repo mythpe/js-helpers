b3a717e4cb6a06a06c84ee6082359b4dfe0825c0
Ahmed Faiz <mythpe@gmail.com>
2024-09-02 10:05:38 +0300

    4.0.97

---
+++ src/components/form/MCkeditorConfig.ts
@@ -0,0 +1,357 @@
+/*
+ * MyTh Ahmed Faiz Copyright © 2016-2024 All rights reserved.
+ * Email: mythpe@gmail.com
+ * Mobile: +966590470092
+ * Website: https://www.4myth.com
+ * Github: https://github.com/mythpe
+ */
+
+import {
+  AccessibilityHelp,
+  Alignment,
+  Autoformat,
+  AutoImage,
+  AutoLink,
+  Autosave,
+  BlockQuote,
+  Bold,
+  CloudServices,
+  Code,
+  CodeBlock,
+  Essentials,
+  FindAndReplace,
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
+  GeneralHtmlSupport,
+  Heading,
+  Highlight,
+  HorizontalLine,
+  ImageBlock,
+  ImageCaption,
+  ImageInline,
+  ImageInsertViaUrl,
+  ImageResize,
+  ImageStyle,
+  ImageTextAlternative,
+  ImageToolbar,
+  ImageUpload,
+  Indent,
+  IndentBlock,
+  Italic,
+  Link,
+  LinkImage,
+  List,
+  ListProperties,
+  Markdown,
+  MediaEmbed,
+  PageBreak,
+  Paragraph,
+  PasteFromMarkdownExperimental,
+  PasteFromOffice,
+  RemoveFormat,
+  SelectAll,
+  SpecialCharacters,
+  SpecialCharactersArrows,
+  SpecialCharactersCurrency,
+  SpecialCharactersEssentials,
+  SpecialCharactersLatin,
+  SpecialCharactersMathematical,
+  SpecialCharactersText,
+  Strikethrough,
+  Style,
+  Subscript,
+  Superscript,
+  Table,
+  TableCaption,
+  TableCellProperties,
+  TableColumnResize,
+  TableProperties,
+  TableToolbar,
+  TextPartLanguage,
+  TextTransformation,
+  Title,
+  TodoList,
+  Underline,
+  Undo
+} from 'ckeditor5'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import arTranslations from 'ckeditor5/translations/ar.js'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import enTranslations from 'ckeditor5/translations/en.js'
+
+export const MCkeditorConfig = {
+  toolbar: {
+    items: [
+      'undo',
+      'redo',
+      '|',
+      'findAndReplace',
+      'selectAll',
+      'textPartLanguage',
+      '|',
+      'heading',
+      'style',
+      '|',
+      'fontSize',
+      'fontFamily',
+      'fontColor',
+      'fontBackgroundColor',
+      '|',
+      'bold',
+      'italic',
+      'underline',
+      'strikethrough',
+      'subscript',
+      'superscript',
+      'code',
+      'removeFormat',
+      '|',
+      'specialCharacters',
+      'horizontalLine',
+      'pageBreak',
+      'link',
+      'mediaEmbed',
+      'insertTable',
+      'highlight',
+      'blockQuote',
+      'codeBlock',
+      '|',
+      'alignment',
+      '|',
+      'bulletedList',
+      'numberedList',
+      'todoList',
+      'outdent',
+      'indent',
+      '|',
+      'accessibilityHelp'
+    ],
+    shouldNotGroupWhenFull: !1
+  },
+  plugins: [
+    AccessibilityHelp,
+    Alignment,
+    Autoformat,
+    AutoImage,
+    AutoLink,
+    Autosave,
+    BlockQuote,
+    Bold,
+    CloudServices,
+    Code,
+    CodeBlock,
+    Essentials,
+    FindAndReplace,
+    FontBackgroundColor,
+    FontColor,
+    FontFamily,
+    FontSize,
+    GeneralHtmlSupport,
+    Heading,
+    Highlight,
+    HorizontalLine,
+    ImageBlock,
+    ImageCaption,
+    ImageInline,
+    ImageInsertViaUrl,
+    ImageResize,
+    ImageStyle,
+    ImageTextAlternative,
+    ImageToolbar,
+    ImageUpload,
+    Indent,
+    IndentBlock,
+    Italic,
+    Link,
+    LinkImage,
+    List,
+    ListProperties,
+    Markdown,
+    MediaEmbed,
+    PageBreak,
+    Paragraph,
+    PasteFromMarkdownExperimental,
+    PasteFromOffice,
+    RemoveFormat,
+    SelectAll,
+    SpecialCharacters,
+    SpecialCharactersArrows,
+    SpecialCharactersCurrency,
+    SpecialCharactersEssentials,
+    SpecialCharactersLatin,
+    SpecialCharactersMathematical,
+    SpecialCharactersText,
+    Strikethrough,
+    Style,
+    Subscript,
+    Superscript,
+    Table,
+    TableCaption,
+    TableCellProperties,
+    TableColumnResize,
+    TableProperties,
+    TableToolbar,
+    TextPartLanguage,
+    TextTransformation,
+    Title,
+    TodoList,
+    Underline,
+    Undo
+  ],
+  fontFamily: {
+    supportAllValues: !0
+  },
+  fontSize: {
+    options: [10, 12, 14, 'default', 18, 20, 22],
+    supportAllValues: !0
+  },
+  heading: {
+    options: [
+      {
+        model: 'paragraph',
+        title: 'Paragraph',
+        class: 'ck-heading_paragraph'
+      },
+      {
+        model: 'heading1',
+        view: 'h1',
+        title: 'Heading 1',
+        class: 'ck-heading_heading1'
+      },
+      {
+        model: 'heading2',
+        view: 'h2',
+        title: 'Heading 2',
+        class: 'ck-heading_heading2'
+      },
+      {
+        model: 'heading3',
+        view: 'h3',
+        title: 'Heading 3',
+        class: 'ck-heading_heading3'
+      },
+      {
+        model: 'heading4',
+        view: 'h4',
+        title: 'Heading 4',
+        class: 'ck-heading_heading4'
+      },
+      {
+        model: 'heading5',
+        view: 'h5',
+        title: 'Heading 5',
+        class: 'ck-heading_heading5'
+      },
+      {
+        model: 'heading6',
+        view: 'h6',
+        title: 'Heading 6',
+        class: 'ck-heading_heading6'
+      }
+    ]
+  },
+  htmlSupport: {
+    allow: [
+      {
+        name: /^.*$/,
+        styles: !0,
+        attributes: !0,
+        classes: !0
+      }
+    ]
+  },
+  image: {
+    toolbar: [
+      'toggleImageCaption',
+      'imageTextAlternative',
+      '|',
+      'imageStyle:inline',
+      'imageStyle:wrapText',
+      'imageStyle:breakText',
+      '|',
+      'resizeImage'
+    ]
+  },
+  link: {
+    addTargetToExternalLinks: !0,
+    defaultProtocol: 'https://',
+    decorators: {
+      toggleDownloadable: {
+        mode: 'manual',
+        label: 'Downloadable',
+        attributes: {
+          download: 'file'
+        }
+      }
+    }
+  },
+  list: {
+    properties: {
+      styles: !0,
+      startIndex: !0,
+      reversed: !0
+    }
+  },
+  placeholder: 'Type or paste your content here! شسي سش',
+  language: 'en',
+  style: {
+    definitions: [
+      {
+        name: 'Article category',
+        element: 'h3',
+        classes: ['category']
+      },
+      {
+        name: 'Title',
+        element: 'h2',
+        classes: ['document-title']
+      },
+      {
+        name: 'Subtitle',
+        element: 'h3',
+        classes: ['document-subtitle']
+      },
+      {
+        name: 'Info box',
+        element: 'p',
+        classes: ['info-box']
+      },
+      {
+        name: 'Side quote',
+        element: 'blockquote',
+        classes: ['side-quote']
+      },
+      {
+        name: 'Marker',
+        element: 'span',
+        classes: ['marker']
+      },
+      {
+        name: 'Spoiler',
+        element: 'span',
+        classes: ['spoiler']
+      },
+      {
+        name: 'Code (dark)',
+        element: 'pre',
+        classes: ['fancy-code', 'fancy-code-dark']
+      },
+      {
+        name: 'Code (bright)',
+        element: 'pre',
+        classes: ['fancy-code', 'fancy-code-bright']
+      }
+    ]
+  },
+  table: {
+    contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+  },
+  translations: [
+    arTranslations,
+    enTranslations
+  ]
+}
